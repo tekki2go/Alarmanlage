20beded9f367bf2e3fa03a9e0195548f0e395eff
tekki2go <therealtekki@gmail.com>
2024-01-05 21:41:16 +0100

    pin changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include <Arduino.h>
 // FS-Bibliotheken
 #include <SPIFFS.h>
-#include <FS.h>
-#include <SD.h>
+//#include <FS.h>
+//#include <SD.h>
 // Netzwerk
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -16,12 +16,15 @@
 #include <MFRC522.h>
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
+#include <Adafruit_BMP280.h>
 // RTC
 #include <RTClib.h>
 #include <NTPClient.h>
 // Telegram-Bot
 #include <UniversalTelegramBot.h>
 #include <ArduinoJson.h>
+// Pin extender
+#include <MCP23017.h>
 
 #pragma endregion
 
@@ -45,10 +48,18 @@ String logging_chat_ids[] = {
 
 #pragma region settings
 
+// Sensor-Sensibilität
+const int pressure_tolerance = 10; //Luftdruck-Toleranz in hPa
+
+// Timings
+int bot_request_delay = 1000;          // Telegram-Bot prüft alle 1000 ms ob neue Nachrichten vorhanden sind
+int bmp280_refresh_delay = 100;        // Luftdruck wird alle 100ms abgefragt
+int dht11_refresh_delay = 2000;        // Temperatur und Luftfeuchtigkeit werden alle 2 sekunden abgefragt
+
 // NTP Server Einstellungen
 const char* ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = 3600;  // GMT-Versatz in Sekunden
-const int daylightOffset_sec = 3600; // Sommerzeit-Versatz in Sekunden
+const long gmtOffset_sec = 3600;      // GMT-Versatz in Sekunden
+const int daylightOffset_sec = 3600;  // Sommerzeit-Versatz in Sekunden
 
 // I2C-Schnittstelle für RTC, LCD und Sensoren (mainWire)
 const int mainSDA = 21;
@@ -72,17 +83,18 @@ const int piezo_pin = 0;                 // Piezo-Piepser
 const int led_red_pin = 2;               // Rote LED
 const int led_green_pin = 15;            // Grüne LED
 
-const int arm_button_pin = 33;           // Taster 1
-const int reset_alarm_button_pin = 32;   // Taster 2
-const int status_button_pin = 35;        // Taster 3
-const int tag_button_pin = 34;           // Taster 4
-const int language_button_pin = 39;      // Taster 5
+const int arm_button_pin = 13;           // Taster 1
+const int reset_alarm_button_pin = 12;   // Taster 2
+const int status_button_pin = 14;        // Taster 3
+const int tag_button_pin = 27;           // Taster 4
+const int language_button_pin = 26;      // Taster 5
 
-const int photoelectric_sensor_pin = 25; // Lichtschranke
-const int radar_sensor_pin = 26;         // Radar
-const int microphone_pin = 27;           // Mikrofon
-const int dht11_pin = 14;                // DHT11
-const int movement_sensor_pin = 12;      // Bewegungsmelder
+const int photoelectric_sensor_pin = 34; // Lichtschranke
+const int radar_sensor_pin = 35;         // Radar
+const int microphone_pin = 32;           // Mikrofon
+const int dht11_pin = 33;                // DHT11
+const int movement_sensor_pin = 25;      // Bewegungsmelder
+
 
 // Log-Einstellungen
 // static const char *TAG = "Alarmanlage"; // Anwendungstag
@@ -100,6 +112,7 @@ RTC_DS1307 rtc;                               // DS1307 RTC
 LiquidCrystal_I2C lcd(0x27, 20, 4);           // I2C LCD
 MFRC522 mfrc522(vspi_ss_pin, rfid_rst_pin);   // RFID-Lesegerät
 DHT dht(dht11_pin, DHT11);                    // DHT-Temperatursensor
+Adafruit_BMP280 bmp;                          // BMP280 Luftdrucksensor
 
 // Twitter Bot
 WiFiClientSecure client;
@@ -114,16 +127,18 @@ TaskHandle_t lcdTask;
 #pragma region variables
 // Variablen
 bool ARMED = false;    // true = scharfgeschaltet
+bool ALARM = false;    // true = alarm ausgelöst
 bool LANGUAGE = false; // false = deutsch, true = englisch
 
 float temperature;     // aktuelle Temperatur
 float humidity;        // aktuelle Luftfeuchtigkeit
+float pressure;        // aktueller Luftdruck
+float height;          // aktuelle Höhe (m)
 
 int status = 0;        // was gerade in der Status-Anzeige gezeigt wird
 // 0 = Temperatur + Luftfeuchtigkeit, 1 = IP
 
-// Telegram-Bot prüft alle 1000ms ob neue Nachrichten vorhanden sind
-int bot_request_delay = 1000;
+// variable für letzten refresh der nachrichten
 unsigned long last_bot_refresh;
 
 #pragma endregion
@@ -154,7 +169,7 @@ String currentTime() {
 // lcd-aktualisierung
 void lcdJob(void * pvParameters) {
     if (LANGUAGE) Serial.println("lcd refresher running on core " + String(xPortGetCoreID()));
-    else Serial.println("LCD-Aktualisierer läuft auf Coree " + String(xPortGetCoreID()));
+    else Serial.println("LCD-Aktualisierer läuft auf Core " + String(xPortGetCoreID()));
 
     lcd.clear();
 
@@ -168,12 +183,16 @@ void lcdJob(void * pvParameters) {
         lcd.setCursor(0, 1);
         switch (status) {
             case 0: 
-                lcd.print(String(temperature) + "°C, ");
+                lcd.print(String(temperature) + "C, ");
                 lcd.print(String(humidity) + "%");
                 break;
             case 1: 
                 lcd.print(("IP: " + String(WiFi.localIP())));
                 break;
+        }
+        if (ALARM) {
+            if (LANGUAGE) Serial.println("Alarm triggered!");
+            else Serial.println("Alarm ausgelöst!");
         }
 
         // Zeile 3: Alarmstatus
@@ -185,6 +204,24 @@ void lcdJob(void * pvParameters) {
         //lcd.print("                    "); // zeile löschen
 
         vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
+void sensorJob(void * pvParameters) {
+    int i = 1;
+    for (;;) {
+        // alle 50ms
+        if (i%5) pressure = bmp.readPressure()/100;
+
+        // alle 500ms
+        if (i%50) {
+            temperature = dht.readTemperature();
+        }
+
+        vTaskDelay(pdMS_TO_TICKS(10)); // 10ms verzögerung
+
+        if (i != 1000) i+=10;
+        else i = 1;
     }
 }
 
@@ -212,6 +249,11 @@ void IRAM_ATTR handleTagButtonInterrupt() {
 
 void IRAM_ATTR handleLanguageButtonInterrupt() {
     LANGUAGE = !LANGUAGE;
+    Serial.println("language");
+}
+
+void IRAM_ATTR handlePhotoelectricInterrupt() {
+    ALARM = true;
 }
 
 #pragma endregion
@@ -409,6 +451,9 @@ void sendLogMessage(String message) {
 
 #pragma endregion
 
+// --------- SENSOREN --------
+
+
 // --------- EINRICHTUNG ---------
 #pragma region setup
 
@@ -469,21 +514,32 @@ void setup() {
     pinMode(tag_button_pin, INPUT_PULLUP);
     pinMode(language_button_pin, INPUT_PULLUP);
 
+    pinMode(photoelectric_sensor_pin, INPUT);
+
     // Interrupts für Buttons hinzufügen
     attachInterrupt(digitalPinToInterrupt(arm_button_pin), &handleArmButtonInterrupt, FALLING);
     attachInterrupt(digitalPinToInterrupt(reset_alarm_button_pin), &handleResetAlarmButtonInterrupt, FALLING);
     attachInterrupt(digitalPinToInterrupt(status_button_pin), &handleStatusButtonInterrupt, FALLING);
     attachInterrupt(digitalPinToInterrupt(tag_button_pin), &handleTagButtonInterrupt, FALLING);
     attachInterrupt(digitalPinToInterrupt(language_button_pin), &handleLanguageButtonInterrupt, FALLING);
+    
+    attachInterrupt(digitalPinToInterrupt(photoelectric_sensor_pin), &handlePhotoelectricInterrupt, FALLING);
+
 
     // Initialisieren des Haupt-I2C-Busses
     //mainWire.begin(mainSDA, mainSCL, mainClockSpeed);
     Wire.begin(mainSDA, mainSCL, mainClockSpeed);
 
-    // Initialisieren des LCD-Displays für mainWire
+    // Initialisieren des LCD-Displays mit mainWire
     lcd.init();
     lcd.clear();
     lcd.backlight();
+
+    // BMP280 mit mainWire
+    if (!bmp.begin()) {
+        if (LANGUAGE) Serial.println("Could not find a valid BMP280 !");
+        else Serial.println("Es konnte kein BMP280 gefunden werden!");
+    }
 
     // Initialisieren des sekundären I2C-Busses
     fastWire.begin(secondarySDA, secondarySCL, secondaryClockSpeed);
@@ -510,11 +566,13 @@ void setup() {
         return;
     }
 
+    // Sensoren
+
 
     rfid_init();
 
 
-    xTaskCreatePinnedToCore(lcdJob, "lcdJob", 10000, NULL, 1, &lcdTask, 0);
+    //xTaskCreatePinnedToCore(lcdJob, "lcdJob", 10000, NULL, 1, &lcdTask, 0);
     delay(250);
     //xTaskCreatePinnedToCore(inputChecker, "inputChecker", 10000, NULL, 1, &inputTask, 0);
     //delay(250);
@@ -532,14 +590,19 @@ void loop() {
     if (millis() > last_bot_refresh + bot_request_delay)  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
 
+    pressure = bmp.readPressure()/100;
+    height = bmp.readAltitude(1013.25);
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
     while(numNewMessages) {
       Serial.println("got response");
       handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
     last_bot_refresh = millis();
-  }
-    delay(1000);
+    }
+    delay(500);
 }
 
 #pragma endregion
